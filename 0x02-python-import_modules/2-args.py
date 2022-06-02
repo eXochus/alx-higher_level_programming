62e1184407dc92f82f6274a4ab7c3d9a0e658fd5
eXochos <odunolaridwan80m@gmail.com>
2022-06-02 20:09:01 +0000

    args and sys

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(arg_num))
     elif arg_num == 1:
         print("{:d} argument.".format(arg_num))
-        print("{:d}: {:s}".format(i, sys.argv[1]))
+        print("{:d}: {:s}".format(i, sys.argv[i]))
     else:
         print("{:d} arguments.".format(arg_num))
-        while i < arg_num:
+        while i <= arg_num:
             print("{:d}: {:s}".format(i, sys.argv[i]))
             i += 1
