189f9cbed76d0c3888d58e190f847b5cd86f8bbe
eXochos <odunolaridwan80m@gmail.com>
2022-06-02 13:54:36 +0000

    modules and cmd line

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from calculation_1 import add, sub, mul, div
-a = 10
-b = 5
-res1 = add(a, b)
-res2 = sub(a, b)
-res3 = mul(a, b)
-res4 = div(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, res1))
-print("{:d} - {:d} = {:d}".format(a, b, res2))
-print("{:d} * {:d} = {:d}".format(a, b, res3))
-print("{:d} / {:d} = {:d}".format(a, b, res4))
+if __name__ == "__main__"
+    from calculation_1 import add, sub, mul, div
+    a = 10
+    b = 5
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
