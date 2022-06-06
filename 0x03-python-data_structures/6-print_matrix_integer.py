e7f3bac70d4ee5da65790f04550a0292d468a05b
eXochos <odunolaridwan80m@gmail.com>
2022-06-06 14:12:13 +0000

    new list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end=" ")
-        print("{:d}".format(row), end=" ")
