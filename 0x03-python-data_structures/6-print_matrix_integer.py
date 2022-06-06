a543b7324e58abc46a88ab691f2cd9b83fa51701
eXochos <odunolaridwan80m@gmail.com>
2022-06-06 14:10:08 +0000

    new list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of ints"""
     for row in matrix:
         for i in row:
-            if i != row[-1]:
-                print("{:d}".format(i), end="")
-            else:
-                print("{:d}".format(i), end="")
-        print()
+            print("{:d}".format(i), end=" ")
+        print("{:d}".format(row), end=" ")
